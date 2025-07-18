3f4ba87a70b9f2b55e816d62082755bc8d975d87
swaraj4cloud <rajjake6@gmail.com>
2025-07-18 09:12:42 -0400

    Make log_parser.py accept filename as argument

--- log_parser.py
+++ log_parser.py
@@ -1,5 +1,13 @@
-with open ('sample.log', 'r')as f:
+import sys
+
+if len (sys.argv) < 2:
+    print("usage: python3 log_parser.py <logfile>")
+    sys.exit(1)
+logfile= sys.argv[1]
+
+with open (logfile, 'r')as f:
   lines = f.readlines()
+
 print ("ERROR lines found :\n")
 for idx, line in enumerate(lines, start=1):
   if 'ERROR'in line:
