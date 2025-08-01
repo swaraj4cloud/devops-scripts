e0f2da150e841d3c5c3bad7099f49dbc0bfc7e60
swaraj4cloud <rajjake6@gmail.com>
2025-07-31 21:01:13 -0400

    Add Splunk-style Error log parser for sys.log

--- log_parser.py
+++ log_parser.py
@@ -1,14 +1,7 @@
-import sys
-
-if len (sys.argv) < 2:
-    print("usage: python3 log_parser.py <logfile>")
-    sys.exit(1)
-logfile= sys.argv[1]
-
-with open (logfile, 'r')as f:
-  lines = f.readlines()
-
-print ("ERROR lines found :\n")
-for idx, line in enumerate(lines, start=1):
-  if 'ERROR'in line:
-    print(f"Line{idx}: {line.strip()}")
+logfile = "syslog.log"
+with open(logfile, 'r') as f:
+    lines = f.readlines()
+print("Filtered error entries:\n")
+for idx, line in enumerate(lines,start=1):
+    if 'ERROR' in line:
+        print(f"Line {idx}:{line.strip()}")
